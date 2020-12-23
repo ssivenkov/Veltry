587306bf28eaed005622bc9b43c9cecaf6c1a193
ssivenkov <sergei_sivenkov@mail.ru>
2020-12-23 23:38:00 +0300

    add simple tablet adaptive design, visual improvements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // tools
+	//cnange color 
 const path1 = document.querySelector('.path1');
 function ChangeOver1() {
 	path1.setAttribute('fill', 'url(#linear-gradient)');
@@ -19,3 +20,22 @@ function ChangeOut2() {
 	document.getElementsByClassName('search')[0].style="background-image: linear-gradient(180deg, #dddddd, #777777)";
 };
 
+	// cnange size
+function setup_for_width(size) {
+	const cartsize = document.querySelector('.cartsize');
+	const searchsize = document.querySelector('.searchsize');
+	if (size.matches) {
+		cartsize.setAttribute('width', '72');
+		cartsize.setAttribute('height', '60');
+		searchsize.setAttribute('width', '72');
+		searchsize.setAttribute('height', '72');
+		} else {
+			cartsize.setAttribute('width', '36');
+			cartsize.setAttribute('height', '30');
+			searchsize.setAttribute('width', '36');
+			searchsize.setAttribute('height', '36');
+		};
+};
+let size = window.matchMedia( "(min-width: 768px)" );
+size.addListener(setup_for_width);
+setup_for_width(size);
