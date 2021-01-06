07371123ecb78394c50df93f6c494f8bbaaaf41f
ssivenkov <sergei_sivenkov@mail.ru>
2021-01-06 17:43:28 +0300

    add simple cart popup window

--- js/script.js
+++ js/script.js
@@ -61,16 +61,16 @@ setup_for_width(size);
 // patallax
 let bg1 = document.querySelector('.mouse-parallax-bg1');
 window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-    bg1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   bg1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
 });
 
 let bg2 = document.querySelector('.mouse-parallax-bg2');
 window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-    bg2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+   let x = e.clientX / window.innerWidth;
+   let y = e.clientY / window.innerHeight;
+   bg2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
 
 // smooth scroll
